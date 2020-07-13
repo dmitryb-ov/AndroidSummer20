8b624f8e22d7df82c88a14df3c6e62f6f4f16b4d
Anvar <anvar00755@mail.ru>
2020-07-13 19:57:48 +0300

    added notification

--- app/src/main/java/com/example/androidsummerproject20/activityToDoList/noteDetail/NoteDetailsActivity.java
+++ app/src/main/java/com/example/androidsummerproject20/activityToDoList/noteDetail/NoteDetailsActivity.java
@@ -1,20 +1,41 @@
 package com.example.androidsummerproject20.activityToDoList.noteDetail;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
 
 import com.example.androidsummerproject20.R;
+import com.example.androidsummerproject20.activityToDoList.ToDoListActivity;
 import com.example.androidsummerproject20.data.App;
+import com.example.androidsummerproject20.main.MainActivity;
 import com.example.androidsummerproject20.notes.Note;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class NoteDetailsActivity extends AppCompatActivity {
 
@@ -25,6 +46,10 @@ public class NoteDetailsActivity extends AppCompatActivity {
 
     //текстовое поле для ввода
     private EditText editText;
+
+    private CheckBox checkBox;
+
+    private Calendar notificationTime = Calendar.getInstance();
 
     //метод для запуска активити
     public static void start(Activity caller, Note note) {
@@ -55,6 +80,12 @@ public class NoteDetailsActivity extends AppCompatActivity {
         setTitle(getString(R.string.note_details_title));
 
         editText = findViewById(R.id.text);
+
+        checkBox = findViewById(R.id.checkBox);
+        checkBox.setText("Установить напоминание");
+
+        Button button = findViewById(R.id.button);
+        button.setText("Выбрать дату");
 
         //getIntent возвращает intent созданный при запуске активити
         //если заметка есть,то есть есть intent
@@ -106,10 +137,76 @@ public class NoteDetailsActivity extends AppCompatActivity {
                     //происходит обновление базы данных, и автоматически livedata всё изменит,
                     //поэтому обратно передавать ничего не нужно
                     //все изменения покажутся на экране
+
+                    if (checkBox.isChecked()) {
+
+                        long time = notificationTime.getTimeInMillis();
+
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Напоминане установлено на: \n" + new Date(time).toString(),
+                                Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+
+                        scheduleNotification(getNotification(
+                                note.text.substring(0, Math.min(note.text.length(), 40))), time);
+                    }
+
                     finish();
                 }
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setTime(View v) {
+        TimePickerDialog.OnTimeSetListener time = new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                notificationTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                notificationTime.set(Calendar.MINUTE, minute);
+                notificationTime.set(Calendar.SECOND, 0);
+            }
+        };
+
+        new TimePickerDialog(this, time,
+                notificationTime.get(Calendar.HOUR_OF_DAY),
+                notificationTime.get(Calendar.MINUTE), true)
+                .show();
+
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                notificationTime.set(Calendar.YEAR, year);
+                notificationTime.set(Calendar.MONTH, monthOfYear);
+                notificationTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            }
+        };
+
+        new DatePickerDialog(this, date,
+                notificationTime.get(Calendar.YEAR),
+                notificationTime.get(Calendar.MONTH),
+                notificationTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+
+    private void scheduleNotification(Notification notification, long delay) {
+
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class)
+                .putExtra(NotificationPublisher.NOTIFICATION_ID, 101)
+                .putExtra(NotificationPublisher.NOTIFICATION, notification);
+
+        PendingIntent pendingIntent =
+                PendingIntent.getBroadcast(this, 0,
+                        notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, delay, pendingIntent);
+    }
+
+    private Notification getNotification(String content) {
+        return new Notification.Builder(this)
+                .setContentTitle("Notification")
+                .setContentText(content)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .build();
     }
 }
