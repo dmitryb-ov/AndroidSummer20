674c3b7d22044c19ec5ea22dcd3e2770ad6e0e21
polyarniik <rus.safiullin.01@mail.ru>
2020-07-13 19:28:41 +0300

    First commit with Notes App

---
+++ app/src/main/java/com/example/androidsummerproject20/models/Note.java
@@ -0,0 +1,49 @@
+package com.example.androidsummerproject20.models;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "text")
+    private String noteText;
+
+    @ColumnInfo(name = "date")
+    private String noteDate;
+
+    public Note() {
+    }
+
+    public Note(String noteText, String noteDate) {
+        this.noteText = noteText;
+        this.noteDate = noteDate;
+    }
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
+
+    public String getNoteDate() {
+        return noteDate;
+    }
+
+    public void setNoteDate(String noteDate) {
+        this.noteDate = noteDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
